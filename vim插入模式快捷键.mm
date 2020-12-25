cca2e17aa82eebc6180661d36b0272230a6925ad
guanyumsheng <hwt220@outlook.com>
2020-12-25 20:33:51 +0800

    md6

---
+++ vim插入模式快捷键.mm
@@ -0,0 +1,153 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="vim插入模式快捷键" FOLDED="false" ID="ID_1363024702" CREATED="1608807068755" MODIFIED="1608807080131" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" NUMBERED="true" BORDER_WIDTH="2.0 px">
+<font NAME="微软雅黑" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="退格" POSITION="right" ID="ID_1157100235" CREATED="1608807080162" MODIFIED="1608807093120">
+<edge COLOR="#ff0000"/>
+<node TEXT="ctrl-h" ID="ID_1899763308" CREATED="1608807093120" MODIFIED="1608807098166"/>
+</node>
+<node TEXT="换行" POSITION="right" ID="ID_499295370" CREATED="1608807099724" MODIFIED="1608807115226">
+<edge COLOR="#0000ff"/>
+<node TEXT="ctrl-m或ctrl-j" ID="ID_699589134" CREATED="1608807115233" MODIFIED="1608807567843"/>
+</node>
+<node TEXT="esc" POSITION="right" ID="ID_1267486399" CREATED="1608807122066" MODIFIED="1608807129815">
+<edge COLOR="#00ff00"/>
+<node TEXT="ctrl-[" ID="ID_799249524" CREATED="1608807129831" MODIFIED="1608807163662"/>
+</node>
+<node TEXT="tab" POSITION="right" ID="ID_1980917019" CREATED="1608807137413" MODIFIED="1608807142949">
+<edge COLOR="#ff00ff"/>
+<node TEXT="ctrl-i" ID="ID_55054633" CREATED="1608807142965" MODIFIED="1608807148579"/>
+</node>
+<node TEXT="退出插入模式" POSITION="right" ID="ID_925307394" CREATED="1608807150130" MODIFIED="1608807183029">
+<edge COLOR="#00ffff"/>
+<node TEXT="ctrl-c" ID="ID_60320168" CREATED="1608807183046" MODIFIED="1608807189779"/>
+</node>
+<node TEXT="增加空格缩进" POSITION="right" ID="ID_1735987217" CREATED="1608807191544" MODIFIED="1608807262620">
+<edge COLOR="#7c0000"/>
+<node TEXT="ctrl-t" ID="ID_1449735715" CREATED="1608807207020" MODIFIED="1608807213053"/>
+</node>
+<node TEXT="减少缩进" POSITION="right" ID="ID_300504796" CREATED="1608807213970" MODIFIED="1608807221727">
+<edge COLOR="#00007c"/>
+<node TEXT="ctrl-d" ID="ID_1034370836" CREATED="1608807221743" MODIFIED="1608807225937"/>
+</node>
+<node TEXT="删除左边的词" POSITION="right" ID="ID_960096301" CREATED="1608807227742" MODIFIED="1608807299716">
+<edge COLOR="#007c00"/>
+<node TEXT="ctrl-w" ID="ID_1334516613" CREATED="1608807299732" MODIFIED="1608807303911"/>
+</node>
+<node TEXT="删除到行首" POSITION="right" ID="ID_187464214" CREATED="1608807304929" MODIFIED="1608807314223">
+<edge COLOR="#7c007c"/>
+<node TEXT="ctrl-u" ID="ID_1465333277" CREATED="1608807314238" MODIFIED="1608807318085"/>
+</node>
+<node TEXT="临时进入命令模式" POSITION="right" ID="ID_185466269" CREATED="1608807318897" MODIFIED="1608807334228">
+<edge COLOR="#007c7c"/>
+<node TEXT="ctrl-o" ID="ID_1143933403" CREATED="1608807334244" MODIFIED="1608807338940"/>
+</node>
+<node TEXT="复制下一行同列字符" POSITION="right" ID="ID_1356753689" CREATED="1608807348438" MODIFIED="1608807489023">
+<edge COLOR="#7c7c00"/>
+<node TEXT="ctrl-e" ID="ID_1667761714" CREATED="1608807384132" MODIFIED="1608807394930"/>
+</node>
+<node TEXT="复制上一行同列字符" POSITION="right" ID="ID_395088380" CREATED="1608807396049" MODIFIED="1608807493070">
+<edge COLOR="#ff0000"/>
+<node TEXT="ctrl-y" ID="ID_1637746099" CREATED="1608807405013" MODIFIED="1608807408422"/>
+</node>
+<node TEXT="向下滚动" POSITION="right" ID="ID_96622223" CREATED="1608807415273" MODIFIED="1608807434364">
+<edge COLOR="#0000ff"/>
+<node TEXT="ctrl-x,ctrl-e" ID="ID_686348877" CREATED="1608807434374" MODIFIED="1608807446025"/>
+</node>
+<node TEXT="向上滚动" POSITION="right" ID="ID_195647363" CREATED="1608807447614" MODIFIED="1608807453931">
+<edge COLOR="#00ff00"/>
+<node TEXT="ctrlx,ctrl-y" ID="ID_455814441" CREATED="1608807453931" MODIFIED="1608807461998"/>
+</node>
+<node TEXT="插入上次插入的内容" POSITION="right" ID="ID_382397884" CREATED="1608807660443" MODIFIED="1608808035235">
+<edge COLOR="#00ffff"/>
+<node TEXT="ctrl-a" ID="ID_390742359" CREATED="1608808035255" MODIFIED="1608808041836"/>
+</node>
+<node TEXT="插入上次插入的内容并切到命令模式" POSITION="right" ID="ID_629052906" CREATED="1608808042975" MODIFIED="1608808072859">
+<edge COLOR="#7c0000"/>
+<node TEXT="ctrl-@" ID="ID_1580107069" CREATED="1608808072859" MODIFIED="1608808079207"/>
+</node>
+<node TEXT="插入寄存器中的内容" POSITION="right" ID="ID_48401335" CREATED="1608808080353" MODIFIED="1608808094128">
+<edge COLOR="#00007c"/>
+<node TEXT="ctrl-r {0-9a-z%#:.-=&quot;}" ID="ID_1556214711" CREATED="1608808094144" MODIFIED="1608808123768"/>
+</node>
+<node TEXT="删除当前行所有缩进" POSITION="right" ID="ID_1196681278" CREATED="1608808127416" MODIFIED="1608808181919">
+<edge COLOR="#007c00"/>
+<node TEXT="0-ctrl-d" ID="ID_1289876315" CREATED="1608808181934" MODIFIED="1608808198761"/>
+</node>
+<node TEXT="删除当前行缩进回复下一行缩进" POSITION="right" ID="ID_1505390199" CREATED="1608808199548" MODIFIED="1608808219187">
+<edge COLOR="#7c007c"/>
+<node TEXT="^-ctrl-d" ID="ID_1919278203" CREATED="1608808219196" MODIFIED="1608808246698"/>
+</node>
+<node TEXT="左移一个词" POSITION="right" ID="ID_1621951868" CREATED="1608808396384" MODIFIED="1608808405786">
+<edge COLOR="#007c7c"/>
+<node TEXT="ctrl-left" ID="ID_100544708" CREATED="1608808405786" MODIFIED="1608808416400"/>
+</node>
+<node TEXT="右移一个词" POSITION="right" ID="ID_165268722" CREATED="1608808417447" MODIFIED="1608808425421">
+<edge COLOR="#7c7c00"/>
+<node TEXT="ctrl-right" ID="ID_415623000" CREATED="1608808425436" MODIFIED="1608808431939"/>
+</node>
+</node>
+</map>
